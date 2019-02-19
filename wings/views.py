a5dbb3c6d370270d126396b34f5608a4457a8783
matsugee <matsugee@gmail.com>
2019-02-19 21:39:13 +0900

    Modified templates to display posts from database.

--- wings/views.py
+++ wings/views.py
@@ -1,4 +1,8 @@
+   
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Schedule
 
 def schedule_list(request):
-    return render(request, 'wings/schedule_list.html', {})
+    schedules = Schedule.objects.filter(date__gte=timezone.now()).order_by('date')
+    return render(request, 'wings/schedule_list.html', {'schedules':schedules})
